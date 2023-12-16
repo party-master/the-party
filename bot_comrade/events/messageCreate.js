510f8cd00c1bbf9a2514c08af9d8497a397084e3
Robert Tanzola <r.c.tanzola@gmail.com>
2023-12-16 04:42:12 -0500

    remove bot_comrade

--- bot_comrade/events/messageCreate.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'messageCreate',
-    once: false,
-    execute(message) {
-        if (message.channel.type == 'dm'){ return; }
-        try { client.functions.get('respond').execute(message); }
-        catch (error) { console.log(error); }
-    }
-}
--- index.js
+++
@@ -1,2 +0,0 @@
-const bot_ourbot = require('./bot_our-bot/index.js');
-const bot_comrade = require('./bot_comrade/index.js');
